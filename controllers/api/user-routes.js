9d4576b34c25f91d04c6952b08a9cbef705a6ffc
Diana Koeswanto <d.koeswanto@gmail.com>
2021-06-03 21:00:25 +1000

    Setting up basic session when user is logged in

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,17 +1,28 @@
 const router = require('express').Router();
-const { User } = require('../../models');
 
 // CREATE new user
 router.post('/', async (req, res) => {
+
   });
 
 // Login - finds the user given username. Sets session if user exists. 
 // Otherwise throw 500
 router.post('/login', async (req, res) => {
+  req.session.save(() => {
+    req.session.loggedIn = true;
+    res.status(200).json({ message: 'You are now logged in!' });
   });
+});
 
 // Logout - if user is signed in, destroy session. Else, throw 404?
 router.post('/logout', (req, res) => {
-  });
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./home-routes');
 const passwordRoutes = require('./password-routes');
 
 router.use('/', homeRoutes);
-router.use('/password', passwordRoutes);
+// router.use('/password', passwordRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
