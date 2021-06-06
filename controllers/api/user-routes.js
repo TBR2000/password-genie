0c4777a9abafd5e4b6ae7fe862ed5957d902b01f
Diana Koeswanto <d.koeswanto@gmail.com>
2021-06-06 10:28:33 +1000

    implemented user login

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const User = require('../../models/User');
 
 // CREATE new user
 router.post('/', async (req, res) => {
@@ -8,10 +9,46 @@ router.post('/', async (req, res) => {
 // Login - finds the user given username. Sets session if user exists. 
 // Otherwise throw 500
 router.post('/login', async (req, res) => {
-  req.session.save(() => {
-    req.session.loggedIn = true;
-    res.status(200).json({ message: 'You are now logged in!' });
-  });
+  try {
+    const user = await User.findOne({ 
+      where: { 
+        email: req.body.usernameOrEmail,
+        $or: [
+          {
+              username: req.body.usernameOrEmail
+          }
+        ]
+      }
+    });
+
+    // console.log(JSON.stringify(user));
+
+    // if (!user) {
+    //   res
+    //     .status(400)
+    //     .json({ message: 'Incorrect username or password, please try again' });
+    //   return;
+    // }
+
+    // const validPassword = await user.checkPassword(req.body.password);
+
+    // if (!validPassword) {
+    //   res
+    //     .status(400)
+    //     .json({ message: 'Incorrect username or password, please try again' });
+    //   return;
+    // }
+
+    req.session.save(() => {
+      // req.session.user_id = user.id;
+      req.session.logged_in = true;
+      
+      res.json({ message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // Logout - if user is signed in, destroy session. Else, throw 404?
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
+// const sequelize = require('./config/connection');
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +14,9 @@ const sess = {
   cookie: {},
   resave: false,
   saveUninitialized: true,
+//   store: new SequelizeStore({
+//     db: sequelize
+//   })
 };
 
 app.use(session(sess));
