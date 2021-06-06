ad9309282c6878d2ddef7d07c6ace28f7f95c59d
Diana Koeswanto <d.koeswanto@gmail.com>
2021-06-06 11:19:27 +1000

    queries user with either username or email. Also implemented create user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,49 +1,57 @@
 const router = require('express').Router();
 const User = require('../../models/User');
+const { Op } = require("sequelize");
 
 // CREATE new user
 router.post('/', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
 
-  });
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(user);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 // Login - finds the user given username. Sets session if user exists. 
 // Otherwise throw 500
 router.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ 
-      where: { 
-        email: req.body.usernameOrEmail,
-        $or: [
-          {
-              username: req.body.usernameOrEmail
-          }
+      where: {
+        [Op.or]: [
+          { email: req.body.usernameOrEmail },
+          { username: req.body.usernameOrEmail }
         ]
       }
     });
 
-    // console.log(JSON.stringify(user));
+    if (!user) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
 
-    // if (!user) {
-    //   res
-    //     .status(400)
-    //     .json({ message: 'Incorrect username or password, please try again' });
-    //   return;
-    // }
+    const validPassword = await user.checkPassword(req.body.password);
 
-    // const validPassword = await user.checkPassword(req.body.password);
-
-    // if (!validPassword) {
-    //   res
-    //     .status(400)
-    //     .json({ message: 'Incorrect username or password, please try again' });
-    //   return;
-    // }
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect username or password, please try again' });
+      return;
+    }
 
     req.session.save(() => {
-      // req.session.user_id = user.id;
+      req.session.user_id = user.id;
       req.session.logged_in = true;
       
-      res.json({ message: 'You are now logged in!' });
+      res.json({ user, message: 'You are now logged in!' });
     });
 
   } catch (err) {
