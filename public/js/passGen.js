2d3f5e1a2542f214f1fec3f516c70c8e27bf2f9a
TBR2000 <timothyrabbidge@live.com.au>
2021-06-14 22:57:00 +1000

    generate password complete, checkboxes working, incorrect login fixed, various other improvements

--- public/js/passGen.js
+++ public/js/passGen.js
@@ -1,11 +1,10 @@
 const randomPassword = async () => {
 //password variables
 const length= document.querySelector('#char_num').value;
-const numbers= document.querySelector('#numCheck').value;
-const symbols= document.querySelector('#symCheck').value;
-const lowercase= document.querySelector('#smCheck').value;
-const uppercase= document.querySelector('#capCheck').value;
-const strict= document.querySelector('#strict').value;
+const numbers= document.querySelector('#numCheck:checked')!==null;
+const symbols= document.querySelector('#symCheck:checked')!==null;
+const lowercase= document.querySelector('#smCheck:checked')!==null;
+const strict= document.querySelector('#strict:checked')!==null;
 
     const response = await fetch('/api/password/generating', {
         method: 'POST',
@@ -14,68 +13,41 @@ const strict= document.querySelector('#strict').value;
             numbers, 
             symbols, 
             lowercase, 
-            uppercase, 
             strict }),
         headers: { 'Content-Type': 'application/json' },
       });  
       if (response.ok) {
-        //response()
+        //password in response
+        const password = (await response.json()).toString();
+        const passwordsave = document.querySelector('#passwordsave')
+          /*output show*/
+        const output = document.querySelector('#password');
+          /* final password */
+        output.value = password;
+        passwordsave.value = output.value;
+               
       } else {
         alert(response.statusText);
-      }
-      //console.log(response)
-    //console.log(lowercase)
-}
+      };
+};
    
-  
-const response = async () => {
-   /*output show*/
-const output = document.querySelector('#password');
-const strength = document.querySelector('#strength').value;
-
-    const password = await fetch ('/api/generator', {
-        method: 'GET',
-        body: JSON.stringify (password),
-        headers: { 'Content-Type': 'application/json' },
-      });
-    
-   /* final password */
-    output.value = password;
-
-   /*check password strength*/
-   if (password.length <= 5){
-      strength.innerHTML = "Weak";
-      strength.style.color = "red";
-   }
-   else if(password.length >= 20){
-       strength.innerHTML = "Very Strong";
-       strength.style.color = "green";
-   }
-   else if(password.length >= 8){
-       strength.innerHTML = "Strong";
-       strength.style.color = "green";
-   }
-   else{
-       strength.innerHTML = "Normal";
-       strength.style.color = "green";
-   }
-}
-
 /*password generate*/
 function generate() {
   const pass_num = document.querySelector('#char_num').value;
 if (pass_num > 0) {
   randomPassword();
-    } 
-}
+    }else{
+      alert('Password must have a valid length')
+    }
+};
 
 /*copy password*/
 const copyFunction = (event) => {
-  const output = document.querySelector('#output');
+  const output = document.querySelector('#password');
     event.preventDefault();
     output.select();
-    output.setSelectionRange(0, 99999)
     document.execCommand("copy");
+    output.setSelectionRange(0, 99999)
     alert("Your password: " + output.value);
 }
 
